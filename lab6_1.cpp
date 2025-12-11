c969a295a9e103016cea5fa1027c719945bb1f38
PPRAME6767 <nidmci9@gmail.com>
2025-12-11 14:03:47 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
+    int i;
+    int x = 0 ;
+    int y = 0 ;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> i;
+    while (i != 0){
+        if(i%2 == 0){
+        x = x+1;
+        }
+        else{
+            y = y+1;
+        }
+       cout << "Enter an integer: ";
+    cin >> i;
+    }
+    
+    cout << "#Even numbers = " << x <<endl;
+    
+    cout << "#Odd numbers = " << y << endl;
     return 0;
 }
